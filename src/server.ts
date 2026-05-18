e61dd57d91b1e21e9196df7ac0ac7c2d6164c1f1
Akash Pradhan <akashpradhan151@gmail.com>
2026-05-18 15:40:52 +0530

    fixed

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,10 @@
 import dotenv from 'dotenv';
-import { Server } from 'socket.io';
-import http from 'http';
 
 dotenv.config();
 
 import app from '@/index';
-import { registerPokerSocket } from './features/poker/poker.events';
 
 const PORT = process.env.PORT || 8080;
-
-const httpServer = http.createServer(app);
-
-const io = new Server(httpServer, {
-  cors: {
-    origin: '*',
-  },
-});
-
-io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
-  });
-});
-
-registerPokerSocket(io);
-
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
